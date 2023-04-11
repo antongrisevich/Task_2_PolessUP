c153a6e62d353b5dbdcd31336333932d5e086088
antongrisevich <tonuo3553356@gmail.com>
2023-04-11 20:45:02 +0300

    task2

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolessUP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 2
+
+            int[] nums = new int[] { 1, 5, 3, 9, 4, 6};
+            int target = 11;
+            for(int i = 0; i< nums.Length; i++)
+            {
+                for(int j = i+1; j< nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        Console.WriteLine($"Индексы чисел, которые в сумме дают число {target} : [{i},{j}]");
+                        break;
+                    }
+                  
+                }
+            }
+        }
+    }
+}
